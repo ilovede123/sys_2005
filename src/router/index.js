f460b8afef365969a1a0c99c04d3a023190917fd
前端摸鱼欧阳锋 <718781500@qq.com>
2020-10-20 09:25:24 +0800

    登入结构完成

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/pages/Home'
+import product from '@/pages/Home/product'
+import Login from "../pages/Login"
+Vue.use(VueRouter)
+const routes = [
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/',
+    component: Home,
+    children: [
+      {
+        path:'/product',
+        component: product
+      }
+    ]
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
